a9634b2539947ed8095aff9925f7a07664479c56
Dennis Eichhorn <spl1nnes.com@googlemail.com>
2023-05-19 02:37:35 +0000

    test fixes + new test data

--- Admin/Install/Navigation.php
+++ Admin/Install/Navigation.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
 
 namespace Modules\Controlling\Admin\Install;
 
-use phpOMS\DataStorage\Database\DatabasePool;
+use phpOMS\Application\ApplicationAbstract;
 
 /**
  * Navigation class.
@@ -36,8 +36,8 @@ class Navigation
      *
      * @since 1.0.0
      */
-    public static function install(string $path, DatabasePool $dbPool) : void
+    public static function install(ApplicationAbstract $app, string $path) : void
     {
-        \Modules\Navigation\Admin\Installer::installExternal($dbPool, ['path' => __DIR__ . '/Navigation.install.json']);
+        \Modules\Navigation\Admin\Installer::installExternal($app, ['path' => __DIR__ . '/Navigation.install.json']);
     }
 }
